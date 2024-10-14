ec569eec03c46fab41e76b93074c611bae667a9c
patradinata <157147084+patradinata@users.noreply.github.com>
2024-10-14 16:30:13 +0700

    Add files via upload

---
+++ html/belajar-html/dom.js
@@ -0,0 +1,33 @@
+document.getElementById("merah").onclick = () => {
+  document.getElementById("text").style.color = "red";
+};
+
+document.getElementById("aqua").onclick = () => {
+  document.getElementById("text").style.color = "aqua";
+};
+
+document.getElementById("hijau").onclick = () => {
+  document.getElementById("text").style.color = "green";
+};
+
+document.getElementById("biru").onclick = () => {
+  document.getElementById("text").style.color = "blue";
+};
+
+document.querySelector(".red").onclick = () => {
+  document.querySelector(".btn").style.backgroundColor = "red";
+};
+
+document.querySelector(".aqua").onclick = () => {
+  document.querySelector(".btn").style.backgroundColor = "aqua";
+};
+
+document.querySelector(".green").onclick = () => {
+  document.querySelector(".btn").style.backgroundColor = "green";
+};
+
+document.querySelector(".blue").onclick = () => {
+  document.querySelector(".btn").style.backgroundColor = "blue";
+};
+
+
---
+++ html/belajar-html/script.js
@@ -0,0 +1,4 @@
+// const buttonLogin = document.querySelector(".btn");
+// buttonLogin.addEventListener(onclick, function () {
+//   alert("https://www.instagram.com/patra_dinata");
+// });
